d4f428dc07c6d940543e8c2878a0b909a8acaacc
AndriyFursa <andriyfursa_12@gmail.com>
2023-02-01 23:15:14 +0200

    button_add

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className="buttons">Click me!</button>
+        <button className="buttons">Click me!</button>
+        <button className="buttons">Click me!</button>
+      </header>
+    </div>
+  )
+}
+
+export default App
